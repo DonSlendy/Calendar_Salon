9f29a4650af88b8f5175703846806aad0346700d
edu <eduandreecorte24@gmail.com>
2025-03-07 15:32:38 -0600

    Se modificaron los estilos de los  botones personalizados para que fueran más agradables visualmente, además se agregó un icono de calendario para poder seleccionar una fecha en específico. También se agregaron los eventos y se está trabajando en la lógica para horas que son nulas, ya sea porque ya no tienen cupo para esa hora o porque en esa hora en específico no se va a trabajar.

--- Calendar_Cliente/src/app/Components/Vistas/agendar-cita/agendar-cita.component.ts
+++ Calendar_Cliente/src/app/Components/Vistas/agendar-cita/agendar-cita.component.ts
@@ -4,11 +4,13 @@ import { CalendarOptions } from '@fullcalendar/core/index.js';
 import dayGridOptions from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPluggin, { DateClickArg } from '@fullcalendar/interaction';
+import { CommonModule } from '@angular/common';
+import { TareasEmpleadosService } from '../../../Services/tareas-empleados.service';
 
 
 @Component({
   selector: 'app-agendar-cita',
-  imports: [FullCalendarModule,],
+  imports: [FullCalendarModule, CommonModule],
   templateUrl: './agendar-cita.component.html',
   styleUrl: './agendar-cita.component.css'
 })
@@ -21,6 +23,7 @@ export class AgendarCitaComponent implements OnInit, AfterViewInit {
   @ViewChild('list_calendar') dateListComponent!: FullCalendarComponent;
 
   horariosActualizados: any = {};
+
 
   Tiempo_Servicios: { [key: string]: number } = {
     "01": 60,//1hora
@@ -47,11 +50,30 @@ export class AgendarCitaComponent implements OnInit, AfterViewInit {
 
   EventosGuardados: any[] = [];
 
-  DiasBloqueados = ['2025-03-14', '2025-03-12', '2025-03-06', '2025-03-07', '2025-03-08', '2025-03-13', '2028-02-29', "2025-03-09"];
+  DiasBloqueados = ['2025-03-14', '2025-03-14', '2025-03-12', '2025-03-06', '2025-03-13', '2028-02-29', "2025-03-09"];
 
   fechaBloqueada!: Boolean;
 
+  constructor(private horariosService: TareasEmpleadosService,) { }
+
   ngOnInit(): void {
+    this.horariosActualizados = this.horariosService.getHorarios();
+
+    const eventos: { title: string; start: any; end: any; color: any; }[] = [];
+
+    Object.keys(this.horariosActualizados).forEach(empleado => {
+      this.horariosActualizados[empleado].forEach((evento: { title: any; start: any; end: any; color: any; }) => {
+        eventos.push({
+          title: `${empleado}: ${evento.title}`,
+          start: evento.start,
+          end: evento.end,
+          color: evento.color
+        });
+      });
+    });
+
+    this.EventosGuardados = eventos;
+
     this.calendarioInLine = {
       locale: "eS",
       initialView: "dayGridWeek",
@@ -123,24 +145,24 @@ export class AgendarCitaComponent implements OnInit, AfterViewInit {
 
       themeSystem: "bootstrap5",
       customButtons: {
+        calendar: {
+          icon: "bi bi-calendar-week"
+        },
         customPrev: {
-          icon: 'bi bi-arrow-left',
+          icon: 'bi bi-chevron-left',
           click: () => {
             //Se crean las constantes para manipular los calendarios
             const dayApi = this.dateListComponent.getApi();
             const calendarApi = this.calendarComponent.getApi();
 
             const currentStartDate = dayApi.getDate();
-            // Calcula la nueva fecha inicial (avanzar un día)
-            // Calcula la nueva fecha inicial (avanzar un día)
+            // Calcula la nueva fecha, empieza con - 0 
             let newStartDate = new Date(currentStartDate.setDate(currentStartDate.getDate() - 0));//Formato Dia Mes 00 00:00:00 GMT-0600 (Hora Est...)...
 
             // Encontrar la siguiente fecha no bloqueada
             do {
-              newStartDate.setDate(newStartDate.getDate() - 1); // Avanza un día
+              newStartDate.setDate(newStartDate.getDate() - 1); // Retrocede un día
             } while (this.DiasBloqueados.includes(newStartDate.toISOString().split("T")[0]));
-
-            console.log("Nueva fecha no bloqueada: ", newStartDate.toISOString().split("T")[0]);
 
             const fechaActual = new Date();
             const fechaEscogida = new Date(newStartDate);
@@ -179,7 +201,7 @@ export class AgendarCitaComponent implements OnInit, AfterViewInit {
           }
         },
         nextPrev: {
-          icon: 'bi bi-arrow-right',
+          icon: 'bi bi-chevron-right',
 
           click: () => {
             const dayApi = this.dateListComponent.getApi();
@@ -195,7 +217,6 @@ export class AgendarCitaComponent implements OnInit, AfterViewInit {
               newStartDate.setDate(newStartDate.getDate() + 1); // Avanza un día
             } while (this.DiasBloqueados.includes(newStartDate.toISOString().split("T")[0]));
 
-            console.log("Nueva fecha no bloqueada: ", newStartDate.toISOString().split("T")[0]);
 
             // Cambia la vista actual al nuevo rango
             dayApi.gotoDate(newStartDate);
@@ -227,10 +248,11 @@ export class AgendarCitaComponent implements OnInit, AfterViewInit {
         }
       },
       headerToolbar: {
-        right: "customPrev,nextPrev",
+        right: "calendar,customPrev,nextPrev",
       },
       firstDay: this.obtenerFechaActual(),
     }
+    const HorasBloqueadas = ["2025-03-07T12:00:00", "2025-03-07T15:00:00"];
 
     this.dateListCalendar = {
       locale: "eS",//Coloca los datos en idioma español 
@@ -242,10 +264,10 @@ export class AgendarCitaComponent implements OnInit, AfterViewInit {
       initialView: 'timeGridDay',
       weekends: true,
       plugins: [dayGridOptions, interactionPluggin, timeGridPlugin],
-      //events: eventos,
-      //eventSources: [{ events: this.EventosGuardados }, { events: this.EventosGuardados2 }],
+      events: eventos,
       selectable: true,
-      //select: this.handleSelect.bind(this),
+      select: this.handleSelect.bind(this),
+
       //eventClick: this.handleEventeClick.bind(this),
       allDaySlot: false,
       slotEventOverlap: false,
@@ -256,26 +278,30 @@ export class AgendarCitaComponent implements OnInit, AfterViewInit {
 
       //Opciones para configurar el formato de como se muestra la hora:
       slotLabelFormat: { hour: "2-digit", minute: "2-digit", hour12: true },
+      selectAllow: function (selectInfo) {
+        // Convierte select.startStr si es necesario y realiza la comparación
+        const inicioSeleccionado = new Date(selectInfo.start).toISOString().split(".")[0];
+        const finSeleccionado = new Date(selectInfo.end).toISOString().split(".")[0];
+
+
+        for (const horaBloqueada of HorasBloqueadas){
+          const bloqueada = new Date(horaBloqueada).toISOString().split(".")[0];
+          if(bloqueada >= inicioSeleccionado && bloqueada < finSeleccionado){
+            return false;
+          }
+        }
+        return true;
+      },
     };
   }
 
-  ngAfterViewInit(): void {/*
-    const diaActual = new Date().toISOString().split("T")[0];
-    if (this.DiasBloqueados.includes(diaActual)) {
-      console.log("Este dia actual está bloqueado");
-      this.fechaBloqueada = true;
-    } else {
-      console.log("Este dia actual NO está bloqueado");
-      this.fechaBloqueada = false;
-
-    }*/
+  ngAfterViewInit(): void {
     setTimeout(() => {
       const calendarApi = this.dateListComponent.getApi();
 
       if (calendarApi) {
         const initialDate = this.iniciarDiaHabil(); // Método para calcular la fecha deseada
         calendarApi.gotoDate(initialDate); // Establece la fecha inicial del calendario
-        console.log('Fecha inicial establecida:', initialDate);
       } else {
         console.error('El API del calendario no está disponible.');
       }
@@ -311,21 +337,123 @@ export class AgendarCitaComponent implements OnInit, AfterViewInit {
     }
   }
 
+  handleSelect(arg: any) {
+    const tiempoMax = this.Tiempo_Servicios[this.servicioEscogido_value];
+    const selectStart = new Date(arg.start);
+    const selectEnd = new Date(arg.end);
+    const duration = (selectEnd.getTime() - selectStart.getTime()) / (1000 * 60);
+
+    const empleados = Object.keys(this.horariosActualizados);
+
+    let empleadoDisponible: string | null = null;
+
+    for (const empleado of empleados) {
+      if (!this.eventoOcupado(arg.start, arg.end, this.horariosActualizados[empleado])) {
+        empleadoDisponible = empleado;
+        break;
+      }
+    }
+
+    if (empleadoDisponible) {
+      // const colorSelect:string=empleadoDisponible.color; 
+      //Estas validaciones son solo para mostrar un mensaje personalizado dependiendo del tiempo escogido, mostrando el tiempo en horas
+      if (duration > tiempoMax || duration < tiempoMax) {
+        alert("El tiempo seleccionado para ese tratamiento no coincide, el tiempo estimado es de" + this.Tiempo_Servicios_Horas[this.servicioEscogido_value]);
+      } else {
+
+        const empleadoColor = this.horariosActualizados[empleadoDisponible][0]?.color || "purple"; // Por defecto azul si no tiene color
+
+        const newEvent = {
+          title: empleadoDisponible + ": " + this.servicioEscogido_text + " - " + "NombreRecibidoPorElBot",
+          start: arg.startStr,
+          end: arg.endStr,
+          color: empleadoColor,
+        };
+
+        this.horariosActualizados[empleadoDisponible] = [
+          ...this.horariosActualizados[empleadoDisponible],
+          newEvent
+        ];
+
+        this.EventosGuardados = [
+          ...this.EventosGuardados,
+          newEvent
+        ];
+
+        // Forzar la actualización del listado
+        this.dateListCalendar = {
+          ...this.dateListCalendar,
+          events: this.EventosGuardados
+        };
+
+        //Para el calendario
+        this.calendarioInLine = {
+          ...this.calendarioInLine,
+          events: this.EventosGuardados
+        }
+      }
+    } else {
+      alert("La hora que deseas ya está ocupada, prueba con otra ");
+      /*
+      // Agregar hora bloqueada si está completamente ocupada
+        if (!horasBloqueadas.includes(arg.startStr)) {
+            horasBloqueadas.push(arg.startStr); // Registra la hora ocupada
+
+            selectAllow: function(selectInfo) {
+        // Deshabilita horas que estén en la lista de horas bloqueadas
+        const horaSeleccionada = selectInfo.startStr;
+        return !horasBloqueadas.includes(horaSeleccionada);
+    },
+        }
+      */
+    }
+  }
+
+  //Función para verificar que no se pueda ingresar una fecha colindante con otro registro
+  eventoOcupado(start: any, end: any, eventos: any[]): boolean {
+    //Se usa la función some para verificar si existe algún dato guardado que choque con las fechas existentes, retorna TRUE
+    return eventos.some(event => {
+      //Estas dos variables obtienen el tiempo de cualquiera de los eventos guardados, tanto inicial como final
+      const eventStart = new Date(event.start).getTime();
+      const eventEnd = new Date(event.end).getTime();
+
+      //Estas dos obtienen el tiempo del nuevo evento a guardar
+      const selectStart = new Date(start).getTime();
+      const selectEnd = new Date(end).getTime();
+
+      //Compara que el tiempo seleccionado no choque con el tiempo final de algún evento existente
+      //También compara si el final del evento seleccionado no comience con el evento inicial de otro
+      return (selectStart < eventEnd && selectEnd > eventStart);
+    });
+  }
+
   iniciarDiaHabil(): string {
     const dayApi = this.dateListComponent.getApi();
     const currentStartDate = dayApi.getDate();
     // Calcula la nueva fecha inicial (avanzar un día)
     // Calcula la nueva fecha inicial (avanzar un día)
-    let newStartDate = new Date(currentStartDate.setDate(currentStartDate.getDate() + 0));//Formato Dia Mes 00 00:00:00 GMT-0600 (Hora Est...)...
+
+    // Si la fecha actual no está bloqueada, devolverla directamente
+    if (!this.DiasBloqueados.includes(currentStartDate.toISOString().split("T")[0])) {
+      return currentStartDate.toISOString().split("T")[0];
+    }
+
+    let newStartDate = new Date(currentStartDate);//Formato Dia Mes 00 00:00:00 GMT-0600 (Hora Est...)...
 
     // Encontrar la siguiente fecha no bloqueada
     do {
       newStartDate.setDate(newStartDate.getDate() + 1); // Avanza un día
     } while (this.DiasBloqueados.includes(newStartDate.toISOString().split("T")[0]));
-
-    console.log("Nueva fecha no bloqueada: ", newStartDate.toISOString().split("T")[0]);
-
     return newStartDate.toISOString().split("T")[0];
+  }
+
+  //Esta función es para obtener de forma simulada el tipo de servicio que agendará la persona
+  serviciosSalon(event: Event) {
+    const selectValue = (event.target as HTMLSelectElement);
+    if (selectValue) {
+      this.servicioEscogido_text = selectValue.options[selectValue.selectedIndex].text;
+      this.servicioEscogido_value = selectValue.value;
+    } 
   }
 
 }
--- Calendar_Cliente/src/app/Services/tareas-empleados.service.ts
+++ Calendar_Cliente/src/app/Services/tareas-empleados.service.ts
@@ -11,15 +11,18 @@ export class TareasEmpleadosService {
     const horarios = {
       "Empleado1": [
         { title: 'Corte de Cabello - Diana Franco', start: '2025-03-08T10:00:00', end: '2025-03-08T11:00:00', color: "#2c3e50" },
-        { title: 'Tinte de Cabelo - Diana Franco', start: '2025-03-08T12:00:00', end: '2025-03-08T13:00:00', color: "#2c3e50" },
+        { title: 'Tinte de Cabello - Diana Franco', start: '2025-03-08T12:00:00', end: '2025-03-08T13:00:00', color: "#2c3e50" },
+        { title: 'Tinte de Cabello - Diana Franco', start: '2025-03-10T15:00:00', end: '2025-03-10T16:00:00', color: "#2c3e50" },
+
       ],
       "Empleado2": [
         { title: 'Pintado de Uñas para Manos - Paola Aparicio', start: '2025-03-08T14:00:00', end: '2025-03-08T15:00:00', color: "#FFA725" },
         { title: 'Exsfoliación para la Cara - Paola Aparicio', start: '2025-03-10T15:00:00', end: '2025-03-10T16:00:00', color: "#FFA725" },
       ],
       "Empleado3": [
-        { title: 'Delineado de Ojos - Alejandra Fernández', start: '2025-03-10T17:00:00', end: '2025-03-10T17:00:00', color: "#C599B6" },
+        { title: 'Delineado de Ojos - Alejandra Fernández', start: '2025-03-10T17:00:00', end: '2025-03-10T18:00:00', color: "#C599B6" },
         { title: 'Maquillaje Facial - Alejandra Fernández', start: '2025-03-27T17:00:00', end: '2025-03-27T20:00:00', color: "#C599B6" },
+        { title: 'Tinte de Cabello - Alejandra Fernández', start: '2025-03-10T15:00:00', end: '2025-03-10T16:00:00', color: "#C599B6" },
       ],
       "Empleado4": [
         { title: 'Masaje de columna - Eduardo Cortez', start: '2025-03-09T00:00:00', end: '2025-03-09T24:00:00', color:"#2DAA9E"},
